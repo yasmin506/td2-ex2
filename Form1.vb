cf8cfcfe7a13a00e2cc8d7736a4ef350b6b21269
yasmin506 <yasmine.mzoughi@isgb.ucar.tn>
2025-11-05 22:57:07 +0100

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Security.Cryptography.X509Certificates
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim x As Integer
+        Dim y As Integer
+        Dim somme As Integer
+        Dim diff As Integer
+        Dim prod As Integer
+        Dim quoti As Integer
+
+
+        x = InputBox("entres x ")
+        y = InputBox(" entre y")
+        somme = x + y
+        diff = x - y
+        prod = x * y
+        quoti = x / y
+
+        MessageBox.Show("x+y=" & somme & vbCrLf & "x-y=" & diff & vbCrLf & "x*y=" & prod & vbCrLf & "x/y=" & quoti)
+
+
+
+
+    End Sub
+End Class
